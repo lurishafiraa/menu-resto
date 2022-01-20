7373a422dba3f99f071d9c420231f5a004f76637
lurishafiraa <lurishafa@gmail.com>
2022-01-21 05:05:53 +0700

    "first commit"

---
+++ src/components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+      <div class="col">
+          <h2><strong>Delicious Food, </strong>in your gadget</h2>
+      </div>
+      <div class="col">
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Hero'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Menu.vue
@@ -0,0 +1,20 @@
+.<template>
+  <div>
+    <Navbar />
+    <h2>Daftar Menu</h2>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: "Menu",
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style>
+</style>
