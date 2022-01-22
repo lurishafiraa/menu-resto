3535cc586e10f114fbfb820c29e292a2fb1dbf6a
lurishafiraa <lurishafa@gmail.com>
2022-01-23 00:09:46 +0700

    input API

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="row">
+  <div class="row mt-4">
       <div class="col">
-          <h2><strong>Delicious Food, </strong>in your gadget</h2>
+          <div class="d-flex h-100">
+              <div class="justify-content-center align-self-center">
+                  <h2><strong>Delicious Food, </strong><br>in Your Gadget</h2>
+                  <p>Ayo pilih makanan favoritmu sekarang</p>
+                  <button class="btn btn-lg btn-success">Pesan</button>
+                  <!-- <router-link class="btn btn-lg btn-success">Pesan</router-link> -->
+              </div>
+          </div>
       </div>
       <div class="col">
+          <img src="../assets/images/Hero.png" width="100%"/>
 
       </div>
   </div>
