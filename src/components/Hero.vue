feee59c049b6fe8de4a8d6aa12e50c8f306bba88
lurishafiraa <lurishafa@gmail.com>
2022-01-24 15:09:04 +0700

    add search menu

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,7 +5,7 @@
               <div class="justify-content-center align-self-center">
                   <h2><strong>Delicious Food, </strong><br>in Your Gadget</h2>
                   <p>Ayo pilih makanan favoritmu sekarang</p>
-                  <button class="btn btn-lg btn-success">Pesan</button>
+                  <router-link class="btn btn-lg btn-success" to="/menu">Pesan</router-link>
                   <!-- <router-link class="btn btn-lg btn-success">Pesan</router-link> -->
               </div>
           </div>
